e3b31c8e41edd7556beec6413ac02bcb4fd590aa
Jonathan Cantu <jonathant.Cantu@gmail.com>
2026-02-13 14:54:01 -0600

    added drag and drop

--- src/app/components/board/board.ts
+++ src/app/components/board/board.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { TaskService } from '../../services/task.service';
+//to make board drag and dropable
+import { CdkDragDrop, DragDropModule,} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [DragDropModule],
   templateUrl: './board.html',
   styleUrl: './board.css',
 })
@@ -13,4 +16,9 @@ export class Board {
   todoTasks=this.taskService.getTasksByStatus('todo');
   inProgressTasks=this.taskService.getTasksByStatus('in-progress');
   doneTasks=this.taskService.getTasksByStatus("done");
+
+  drop(event: CdkDragDrop<any>, newStatus: 'todo' | 'in-progress' | 'done') {
+  const task = event.item.data;
+  this.taskService.moveTask(task.id, newStatus);
+}
 }
